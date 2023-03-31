d316082426021ffa581351858fb260cb2134efad
Chris <74742645+FrocketGaming@users.noreply.github.com>
2023-03-31 10:24:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -218,7 +218,6 @@ def book_stats():
         title="Books Read per Month",
         dark_theme=True)
 
-    fig.update_yaxes(title_text="Pages Read")
     fig.update_layout(paper_bgcolor="#0e1117", font_size=14,
                       margin=dict(t=90))
     fig2.update_layout(paper_bgcolor="#0e1117",
